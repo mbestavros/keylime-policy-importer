8385157dd1d7b6515ff3c0c74244667d37f614ac
Mark Bestavros <mbestavr@redhat.com>
2022-08-15 19:15:11 -0400

    Add legacy Keylime allowlist conversion support

--- importer.py
+++ importer.py
@@ -15,23 +15,63 @@ EMPTY_ALLOWLIST = {
     "ima": {"ignored_keyrings": [], "log_hash_alg": "sha1"},
 }
 
-# Reads an in-toto .link file present at link_path and converts it to a Keylime policy.
-def convert_link(link_path):
-    policy = copy.deepcopy(EMPTY_ALLOWLIST)
-    policy["meta"]["timestamp"] = str(datetime.datetime.now())
-    policy["meta"]["generator"] = "keylime-policy-importer"
+# Converts legacy allowlists to new JSON format.
+def convert_legacy_allowlist(allowlist_path):
+    with open(allowlist_path, "r") as f:
+        alist_raw = f.read()
+
+    alist = copy.deepcopy(EMPTY_ALLOWLIST)
+    alist["meta"]["timestamp"] = str(datetime.datetime.now())
+    alist["meta"]["generator"] = "keylime-legacy-format-upgrade"
+
+    for line in alist_raw.splitlines():
+        line = line.strip()
+        if len(line) == 0:
+            continue
+
+        pieces = line.split(None, 1)
+        if not len(pieces) == 2:
+            print("Line in Allowlist does not consist of hash and file path: %s", line)
+            continue
+
+        (checksum_hash, path) = pieces
+
+        if path.startswith("%keyring:"):
+            entrytype = "keyrings"
+            path = path[len("%keyring:") :]  # remove leading '%keyring:' from path to get keyring name
+        else:
+            entrytype = "hashes"
+
+        if path in alist[entrytype]:
+            alist[entrytype][path].append(checksum_hash)
+        else:
+            alist[entrytype][path] = [checksum_hash]
+    return alist
+
+# Reads an in-toto .link file present at link_path and converts it to a Keylime
+# policy, or appends to an existing policy if provided.
+def convert_link(link_path, policy=None):
+    if not policy:
+        policy = copy.deepcopy(EMPTY_ALLOWLIST)
+        policy["meta"]["timestamp"] = str(datetime.datetime.now())
+        policy["meta"]["generator"] = "keylime-policy-importer"
 
     link = metadata.Metablock.load(link_path)
     artifacts = link.signed.products
 
     for path in artifacts.keys():
-        policy["hashes"][path] = artifacts[path]["sha256"]
+        entrytype = "hashes"
+        hash = artifacts[path]["sha256"]
+        if path in policy[entrytype]:
+            policy[entrytype][path].append(hash)
+        else:
+            policy[entrytype][path] = [hash]
 
     return policy
 
 if __name__ == "__main__":
-    link_path = "example-link.link"
-    policy = convert_link(link_path)
-    print(policy)
+    link_path = "artifacts/example-link.link"
+    policy = convert_link(link_path, policy=convert_legacy_allowlist("artifacts/allowlist.txt"))
+    print(json.dumps(policy))
     with open("keylime-policy.txt", "w") as f:
         f.write(json.dumps(policy))
